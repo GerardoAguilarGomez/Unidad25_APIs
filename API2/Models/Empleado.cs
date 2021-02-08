685ba3e8ed67bc25203b421921e578ff1c3d88ff
GerardoAguilarGomez <gerardo.aguilar@grupocastilla.es>
2021-02-08 16:18:48 +0100

    api2

---
+++ API2/Models/Empleado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API2
+{
+    public class Empleado
+    {
+        public string Dni { get; set; }
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public int CodigoDepartamento { get; set; }
+
+        public Departamento departamento { get; set; }
+    }
+}
